87e6d08615d053a2cb1f7527ec20e5ccc5c44e18
revolutionpropertymaintenance <revolutionpropertymaintenance1@gmail.com>
2025-07-10 17:51:37 +0100

    Center the navigation bar

--- src/components/Layout/Navigation.tsx
+++ src/components/Layout/Navigation.tsx
@@ -23,7 +23,7 @@ const Navigation: React.FC = () => {
 
   return (
     <nav className="bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 flex justify-center">
         <div className="flex space-x-8">
           {navItems.map(({ path, label, icon: Icon }) => (
             <NavLink
