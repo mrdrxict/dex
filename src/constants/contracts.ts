f0694e55695966b43f61ed916fdbf94977d7a964
revolutionpropertymaintenance <revolutionpropertymaintenance1@gmail.com>
2025-07-10 17:53:07 +0100

    Fix farming contract addresses

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -15,9 +15,9 @@ export const CONTRACT_ADDRESSES: Record<number, ContractAddresses> = {
     router: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
     bridge: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
     staking: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
-    farming: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707',
-    dxbToken: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
-    weth: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707'
+    farming: '0x0165878A594ca255338adfa4d48449f69242Eb8F',
+    dxbToken: '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853',
+    weth: '0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6'
   },
   // Ethereum Mainnet
   1: {
